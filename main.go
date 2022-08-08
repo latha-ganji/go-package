2ad05d5b286d6a9092e2ca63ecc42353c142d0bb
Latha Ganji <latha.ganji@ibm.com>
2022-08-08 19:08:41 +0530

    testing

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
-package main
+package gopackage
 
 import (
 	"fmt"
-
-	a "github.com/latha-ganji/go-package/testPackage"
+	//a "github.com/latha-ganji/go-package/testPackage"
 )
 
 func main() {
 	fmt.Println("hello")
 	fmt.Println(Add(10, 20))
 
-	var message = a.TestPackageLib("Hello Latha")
-	fmt.Println(message)
+	//var message = a.TestPackageLib("Hello Latha")
+	//fmt.Println(message)
 }
 
 func Add(a, b int) int {
